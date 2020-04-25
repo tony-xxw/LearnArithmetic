3df88bf4173fd95adebbf58f471b303606de0fad
wynne <a664966415@gmail.com>
2020-04-25 23:55:15 +0800

    循环链表,双链表

--- src/chapter3/linked/Node.java
+++ src/chapter3/linked/Node.java
@@ -7,13 +7,27 @@ public class Node {
     public Node next = null;// 节点的引用，指向下一个节点
     public int data;// 节点的对象，即内容
     public String content;
+    public Node pre;
 
     public Node(int data) {
         this.data = data;
     }
 
+
+
     public Node(String content) {
         this.content = content;
+    }
+
+    public Node( String content,Node next) {
+        this.next = next;
+        this.content = content;
+    }
+
+    public Node(Node next, String content, Node pre) {
+        this.next = next;
+        this.content = content;
+        this.pre = pre;
     }
 
     public Node getNext() {
