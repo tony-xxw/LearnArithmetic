75655bbd5c49e22f5acd2319f6959b29d2fcbccb
tony-xxw <a664966415@gmail.com>
2020-05-22 10:41:13 +0800

    annotation

--- src/dagger/annotations/AnnotationsMain.java
+++ src/dagger/annotations/AnnotationsMain.java
@@ -1,30 +1,43 @@
 package dagger.annotations;
 
-import java.lang.annotation.Inherited;
-import java.lang.annotation.Retention;
-import java.lang.annotation.RetentionPolicy;
+import org.omg.CORBA.PUBLIC_MEMBER;
+
+import java.lang.annotation.*;
+import java.lang.reflect.Field;
+import java.lang.reflect.Method;
+import java.util.concurrent.TimeoutException;
 
 public class AnnotationsMain {
 
 
-    public static void main(String[] args) {
+    @TestAnnotation(id = 522, msg = "annotation")
+//注解
+    class Test {
+
+        @Check(10)
+        int value;
+
+        @TestAnnotation
+        @Parent()
+        public void say() {
+        }
+
+        @SuppressWarnings("deprecated")
+        public void test1() {
+            System.out.println(value);
+        }
 
     }
 
-
-    @TestAnnotation //注解
-    public void say() {
-
-    }
     /**
-      @RetentionPolicy.RUNTIME 注解可以在运行中得到保留
-      @Retention(RetentionPolicy.SOURCE) 注解只保留在源码阶段,源码执行完就会忽略
-      @Retention(RetentionPolicy.CLASS) 注解只会保留在编译阶段, 编译结束就会忽略
+     * @RetentionPolicy.RUNTIME 注解可以在运行中得到保留
+     * @Retention(RetentionPolicy.SOURCE) 注解只保留在源码阶段, 源码执行完就会忽略
+     * @Retention(RetentionPolicy.CLASS) 注解只会保留在编译阶段, 编译结束就会忽略
      */
     @Retention(RetentionPolicy.RUNTIME)
 
 /**
- @Documented  将注解中的元素包含到javadoc中
+ @Documented 将注解中的元素包含到javadoc中
 
  @Target 指定注解运用的地方
  ElementType.ANNOTATION_TYPE 可以给一个注解进行注解
@@ -35,10 +48,14 @@ public class AnnotationsMain {
  ElementType.PACKAGE 可以给一个包进行注解
  ElementType.PARAMETER 可以给一个方法内的参数进行注解
  ElementType.TYPE 可以给一个类型进行注解，比如类、接口、枚举
-
- @Inherited 当父类被Inherited注解过,子类又无其他注解,则子类继承父类注解
-
  @Repeatable 1.8加入 可重复注解
+
+
+
+ @Deprecated 过时注解
+ @SuppressWarnings 接触警告 (SuppressWarnings(deprecated))
+ @SafeVarargs 安全类注解
+ @Functionallnterface 函数接口注解
  */
     public @interface TestAnnotation {
 
@@ -48,10 +65,72 @@ public class AnnotationsMain {
 
     }
 
+    /**
+     * @Inherited 当父类被Inherited注解过, 子类又无其他注解, 则子类继承父类注解
+     */
     @Inherited
     @Retention(RetentionPolicy.RUNTIME)
-    @interface Parent{}
+    @interface Parent {
+    }
+
+    @Parent
+    public class A {
+    }
 
 
+    public @interface Check {
+        int value();
+    }
 
+    public class B extends A {
+    }
+
+    @interface Persons {
+        Person[] value();
+    }
+
+    @Repeatable(Persons.class)
+    @interface Person {
+        String role() default "";
+    }
+
+    @Person(role = "artist")
+    @Person(role = "coder")
+    @Person(role = "PM")
+    public class SuperMan {
+
+    }
+
+
+    public static void main(String[] args) {
+        boolean hasAnnotation = TestAnnotation.class.isAnnotation();
+
+        if (hasAnnotation) {
+            TestAnnotation testAnnotation = Test.class.getAnnotation(TestAnnotation.class);
+            System.out.println("id: " + testAnnotation.id());
+            System.out.println("msg: " + testAnnotation.msg());
+        }
+
+
+        try {
+            Field a = Test.class.getDeclaredField("value");
+            a.setAccessible(true);
+            Check check = a.getAnnotation(Check.class);
+            if (check != null) {
+                System.out.println("a: " + check.value());
+            }
+
+            Method method = Test.class.getDeclaredMethod("say");
+            if (method != null) {
+                Annotation[] annotations = method.getAnnotations();
+                for (int i = 0; i < annotations.length; i++) {
+                    System.out.println("method: " + annotations[i].annotationType().getSimpleName());
+                }
+            }
+        } catch (NoSuchFieldException e) {
+            e.printStackTrace();
+        } catch (NoSuchMethodException e) {
+            e.printStackTrace();
+        }
+    }
 }
