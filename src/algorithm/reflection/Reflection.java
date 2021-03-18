0d9e90e48e0f1772ca81ef02ec1341b0a7e2e780
tony-xxw <a664966415@gmail.com>
2021-03-18 17:36:41 +0800

    menu change
    (cherry picked from commit 49128a1d2a6a081020a5fab886190648cfa110a7)

--- src/algorithm/reflection/Reflection.java
+++ src/algorithm/reflection/Reflection.java
@@ -1,4 +1,66 @@
 package algorithm.reflection;
 
+import java.lang.reflect.Field;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+
 public class Reflection {
+    public static void main(String[] args) throws NoSuchMethodException, ClassNotFoundException, NoSuchFieldException, IllegalAccessException, InstantiationException, InvocationTargetException {
+//        Class<?> c = methodClass.class;
+//        Class<?> c = new methodClass().getClass();
+        Class<?> c = Class.forName("algorithm.reflection.methodClass");
+        Method[] declaredMethods = c.getDeclaredMethods();
+        Method[] methods = c.getMethods();
+        Method add = c.getMethod("add", int.class, int.class);
+        for (Method declaredMethod : declaredMethods) {
+            System.out.println("declaredMethod: " + declaredMethod.toString());
+        }
+
+        for (Method method : methods) {
+            System.out.println("method: " + method);
+        }
+
+        System.out.println("add: " + add.toString());
+        Object o = c.newInstance();
+        System.out.println("add result: "+add.invoke(o, 1, 4));
+
+        System.out.println("-------------------------");
+
+        Field[] declaredFields = c.getDeclaredFields();
+        for (Field declaredField : declaredFields) {
+            System.out.println("declaredField: " + declaredField.toString());
+        }
+        Field[] fields = c.getFields();
+        for (Field field : fields) {
+            System.out.println("field: " + field.toString());
+        }
+        System.out.println("singleField" + c.getField("str"));
+
+    }
+}
+
+class parent {
+    int x = 10;
+
+    private void test() {
+
+    }
+
+    public void test1() {
+
+    }
+}
+
+class methodClass extends parent {
+    private final int test = 3;
+    public String str = "wxx";
+
+    public int add(int a, int b) {
+        return a + b;
+    }
+
+    public int sub(int a, int b) {
+        return a - b;
+    }
+
 }
